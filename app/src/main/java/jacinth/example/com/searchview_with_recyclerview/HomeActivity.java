bbc3849c99fb66f2dfe15aa5d687f5a8847daa85
Navjacinth Mathew <jacinth9@gmail.com>
2017-05-04 13:33:45 +0530

    Added new project

---
+++ app/src/main/java/jacinth/example/com/searchview_with_recyclerview/HomeActivity.java
@@ -0,0 +1,13 @@
+package jacinth.example.com.searchview_with_recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
