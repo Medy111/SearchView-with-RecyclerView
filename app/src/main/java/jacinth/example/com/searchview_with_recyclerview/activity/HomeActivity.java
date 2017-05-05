252b7aa091a2f4b212e9b78972e14e814cc40b66
Navjacinth Mathew <jacinth9@gmail.com>
2017-05-05 06:22:19 +0530

    -Added functionality to show list

--- app/src/main/java/jacinth/example/com/searchview_with_recyclerview/activity/HomeActivity.java
+++ app/src/main/java/jacinth/example/com/searchview_with_recyclerview/activity/HomeActivity.java
@@ -3,24 +3,49 @@ package jacinth.example.com.searchview_with_recyclerview.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import jacinth.example.com.searchview_with_recyclerview.R;
+import jacinth.example.com.searchview_with_recyclerview.adapter.BrandNameAdapter;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.search_view_brand)
+    SearchView searchViewBrand;
+    @BindView(R.id.recycler_view_brand)
+    RecyclerView recyclerViewBrand;
+
+    private BrandNameAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
         getResourceAsList();
+        setAdapter();
     }
 
     /*Method to get string array from resources as ArrayList*/
     @NonNull
     private List<String> getResourceAsList() {
         return Arrays.asList(getResources().getStringArray(R.array.car_brands));
+    }
+
+    private void setAdapter() {
+        adapter = new BrandNameAdapter(getResourceAsList());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this);
+
+        recyclerViewBrand.setLayoutManager(layoutManager);
+
+        recyclerViewBrand.setAdapter(adapter);
     }
 }
--- app/src/main/java/jacinth/example/com/searchview_with_recyclerview/adapter/BrandNameAdapter.java
+++ app/src/main/java/jacinth/example/com/searchview_with_recyclerview/adapter/BrandNameAdapter.java
@@ -1,8 +1,53 @@
 package jacinth.example.com.searchview_with_recyclerview.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jacinth.example.com.searchview_with_recyclerview.R;
+
 /**
  * Created by Navjacinth Mathew on 5/5/2017.
  */
 
-public class BrandNameAdapter {
+public class BrandNameAdapter extends RecyclerView.Adapter<BrandNameAdapter.BrandViewHolder> {
+
+    private List<String> dataList;
+
+    public BrandNameAdapter(List<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @Override
+    public BrandViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_list_item, parent, false);
+        return new BrandViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(BrandViewHolder holder, int position) {
+        holder.txtCityname.setText(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+
+    class BrandViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtCityname;
+
+        BrandViewHolder(View itemView) {
+            super(itemView);
+            txtCityname = (TextView) itemView.findViewById(R.id.txt_brand_name);
+        }
+    }
 }
