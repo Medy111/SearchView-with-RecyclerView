c5dd43cc4a7a11972dbbc6ee3d1a0c993503045e
Navjacinth Mathew <jacinth9@gmail.com>
2017-05-07 13:41:17 +0530

    -Added functionality search and filter list based on search query

--- app/src/main/java/jacinth/example/com/searchview_with_recyclerview/activity/HomeActivity.java
+++ app/src/main/java/jacinth/example/com/searchview_with_recyclerview/activity/HomeActivity.java
@@ -7,20 +7,15 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import jacinth.example.com.searchview_with_recyclerview.R;
 import jacinth.example.com.searchview_with_recyclerview.adapter.BrandNameAdapter;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-    @BindView(R.id.search_view_brand)
-    SearchView searchViewBrand;
-    @BindView(R.id.recycler_view_brand)
-    RecyclerView recyclerViewBrand;
+    private SearchView searchViewBrand;
+    private RecyclerView recyclerViewBrand;
 
     private BrandNameAdapter adapter;
 
@@ -28,24 +23,91 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        ButterKnife.bind(this);
-        getResourceAsList();
+        initViews();
+        getDataAsList();
         setAdapter();
+    }
+
+    private void initViews() {
+        searchViewBrand = (SearchView) findViewById(R.id.search_view_brand);
+        recyclerViewBrand = (RecyclerView) findViewById(R.id.recycler_view_brand);
+
+        searchViewBrand.setOnQueryTextListener(this);
     }
 
     /*Method to get string array from resources as ArrayList*/
     @NonNull
-    private List<String> getResourceAsList() {
-        return Arrays.asList(getResources().getStringArray(R.array.car_brands));
+    private ArrayList<String> getDataAsList() {
+        ArrayList<String> dataList = new ArrayList<>();
+        dataList.add("Alfa Romeo");
+        dataList.add("Aston Martin");
+        dataList.add("Audi");
+        dataList.add("Bentley");
+        dataList.add("Benz");
+        dataList.add("BMW");
+        dataList.add("Bugatti");
+        dataList.add("Cadillac");
+        dataList.add("Chevrolet");
+        dataList.add("Chrysler");
+        dataList.add("Citroen");
+        dataList.add("Corvette");
+        dataList.add("Dacia");
+        dataList.add("Datsun");
+        dataList.add("Dodge");
+        dataList.add("Ferrari");
+        dataList.add("Fiat");
+        dataList.add("Ford");
+        dataList.add("Honda");
+        dataList.add("Hummer");
+        dataList.add("Hyundai");
+        dataList.add("Jaguar");
+        dataList.add("Jeep");
+        dataList.add("Lamborghini");
+        dataList.add("Land Rover");
+        dataList.add("Lexus");
+        dataList.add("Lincoln");
+        dataList.add("Lotus");
+        dataList.add("Martini");
+        dataList.add("Maserati");
+        dataList.add("Mazda");
+        dataList.add("McLaren");
+        dataList.add("Mercedes");
+        dataList.add("Mercedes Benz");
+        dataList.add("Mini");
+        dataList.add("Mitsubishi");
+        dataList.add("Nissan");
+        dataList.add("Opel");
+        dataList.add("Pontiac");
+        dataList.add("Porsche");
+        dataList.add("Renault");
+        dataList.add("Rolls Royce");
+        dataList.add("Rover");
+        dataList.add("Skoda");
+        dataList.add("Spyker");
+        dataList.add("Subaru");
+        dataList.add("Suzuki");
+        dataList.add("Toyota");
+        dataList.add("Volkswagen");
+        dataList.add("Volvo");
+        return dataList;
     }
 
+    /*Method to initialise and set the adapter*/
     private void setAdapter() {
-        adapter = new BrandNameAdapter(getResourceAsList());
-
+        adapter = new BrandNameAdapter(getDataAsList());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this);
-
         recyclerViewBrand.setLayoutManager(layoutManager);
-
         recyclerViewBrand.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.getFilter().filter(newText);
+        return false;
     }
 }
--- app/src/main/java/jacinth/example/com/searchview_with_recyclerview/adapter/BrandNameAdapter.java
+++ app/src/main/java/jacinth/example/com/searchview_with_recyclerview/adapter/BrandNameAdapter.java
@@ -4,34 +4,39 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import jacinth.example.com.searchview_with_recyclerview.R;
 
 /**
- * Created by Navjacinth Mathew on 5/5/2017.
+ * Created by Navjacinth Mathew on 5/4/2017.
  */
 
-public class BrandNameAdapter extends RecyclerView.Adapter<BrandNameAdapter.BrandViewHolder> {
+public class BrandNameAdapter extends RecyclerView.Adapter<BrandNameAdapter.CityViewHolder> implements Filterable {
 
-    private List<String> dataList;
+    private ArrayList<String> dataList;
+    private ArrayList<String> parentList;
 
-    public BrandNameAdapter(List<String> dataList) {
+    public BrandNameAdapter(ArrayList<String> dataList) {
         this.dataList = dataList;
+        if (!dataList.isEmpty()) {
+            parentList = new ArrayList<>(dataList);
+        }
     }
 
     @Override
-    public BrandViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public CityViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.row_list_item, parent, false);
-        return new BrandViewHolder(view);
+        return new CityViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(BrandViewHolder holder, int position) {
+    public void onBindViewHolder(CityViewHolder holder, int position) {
         holder.txtCityname.setText(dataList.get(position));
     }
 
@@ -40,14 +45,65 @@ public class BrandNameAdapter extends RecyclerView.Adapter<BrandNameAdapter.Bran
         return dataList.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        return new CityFilter(this, parentList);
+    }
 
-    class BrandViewHolder extends RecyclerView.ViewHolder {
+
+    class CityViewHolder extends RecyclerView.ViewHolder {
 
         TextView txtCityname;
 
-        BrandViewHolder(View itemView) {
+        CityViewHolder(View itemView) {
             super(itemView);
             txtCityname = (TextView) itemView.findViewById(R.id.txt_brand_name);
+        }
+    }
+
+    private static class CityFilter extends Filter {
+
+        private final BrandNameAdapter adapter;
+
+        private final ArrayList<String> originalList;
+
+        private final ArrayList<String> filteredList;
+
+        private CityFilter(BrandNameAdapter adapter, ArrayList<String> originalList) {
+            super();
+            this.adapter = adapter;
+            this.originalList = new ArrayList<>(originalList);
+            this.filteredList = new ArrayList<>();
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            filteredList.clear();
+            final FilterResults results = new FilterResults();
+
+            if (constraint.length() == 0) {
+                filteredList.addAll(originalList);
+            } else {
+                final String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (final String city : originalList) {
+
+                    if ((city.toLowerCase()).contains(filterPattern)) {
+                        filteredList.add(city);
+                    }
+                }
+            }
+            results.values = filteredList;
+            results.count = filteredList.size();
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+
+            adapter.dataList.clear();
+            adapter.dataList.addAll((ArrayList<String>) filterResults.values);
+            adapter.notifyDataSetChanged();
         }
     }
 }
